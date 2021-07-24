04539a77837c0620322882d5b9ce8788cfb94e12
DariaGori <daksataksa@gmail.com>
2021-07-24 12:49:11 +0300

    Pattern Intro init

---
+++ PatternIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PatternIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Pattern Training
+
+This is a repository containing the projects following the exercises given and described in the Head First Design Patterns book.
+
